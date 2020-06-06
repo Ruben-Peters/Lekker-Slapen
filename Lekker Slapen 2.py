608b34c31a4c6eaa27c4ef55f7eb60130d3be0ed
Ruben Peters <Ruben-Peters@live.nl>
2020-06-06 15:24:59 +0000

    added start menu

--- Lekker Slapen 2.py
+++ Lekker Slapen 2.py
@@ -9,9 +9,11 @@ display_height = 600
 
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
+red = (200,0,0)
+green = (0,200,0)
 blue = (0,0,255)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
 
 block_color = (53,115,255)
 
@@ -55,6 +57,51 @@ def message_display(text):
 
 def crash():
     message_display('Lekker Slapen')
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+    
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        gameDisplay.fill(black)
+        largeText = pygame.font.Font('freesansbold.ttf', 90)
+        TextSurf, TextRect = text_objects("League of Slapen", largeText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        
+        button("Start", 150,450,100,50, green,bright_green, game_loop)
+        button("Quit", 550,450,100,50, red,bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
 
 def game_loop():
 
@@ -125,6 +172,7 @@ def game_loop():
         pygame.display.update()
         clock.tick(60)
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
